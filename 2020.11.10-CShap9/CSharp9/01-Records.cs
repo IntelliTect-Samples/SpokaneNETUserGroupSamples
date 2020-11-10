e6a40b9dcc085474205082b607121f11af282c1f
Kevin Bost <kitokeboo@gmail.com>
2020-11-09 16:53:17 -0800

    C# 9 starting base

---
+++ 2020.11.10-CShap9/CSharp9/01-Records.cs
@@ -0,0 +1,11 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Xunit;
+
+namespace CSharp9
+{
+    public class Records
+    {
+        
+    }
+}
---
+++ 2020.11.10-CShap9/CSharp9/04-PatternMatching.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CSharp9
+{
+    public class PatternMatchingTests
+    {
+
+        [Fact]
+        public void NegatedPatterns()
+        {
+            
+        }
+    }
+}
