26c6be83d2d234a388b44b97eddc838cd7e45fc4
Kevin Bost <kitokeboo@gmail.com>
2020-11-10 17:16:33 -0800

    Adding C# 9 guidelines

--- 2020.11.10-CShap9/CSharp9/07-Guidelines.cs
+++ 2020.11.10-CShap9/CSharp9/07-Guidelines.cs
@@ -1,11 +1,10 @@
-
- /*
- * Positional parameter should be PascalCase
- * Positional parameters automatically generate readonly (init) properties
- */
-
 /*
- * init Only allowed in place of setter on a property
- * init is not really improvement to nullable reference type
- */
-
+ * - CONSIDER using records over classes
+ *     Unless:
+ *     - Inheritance chains with classes are required
+ *     - The identity of the object is not based of the value of all its fields
+ * - AVOID positional records when validation of positional members is required
+ * - CONSIDER using init properties instead of readonly properties for optional values
+ * - DO define a constructor to allow setting init properties for backwards compatibility with pre-C# 9
+ * - DO name record positional parameter with PascalCase
+*/
