e4a151135152cff24528e4e0fb9e357b94166849
Kevin Bost <kitokeboo@gmail.com>
2021-04-09 00:06:22 -0700

    WIP

---
+++ 2021.04.13-System.CommandLine/FileConverter/FileConverter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace FileConverter
+{
+    class Program
+    {
+        /// <summary>
+        /// Demo app for converting files
+        /// </summary>
+        /// <param name="inputFile">The file to convert</param>
+        /// <param name="outputDirectory">The output directory to covert files into.</param>
+        /// <param name="ouputType">The conversion type.</param>
+        static void Main(
+            FileInfo inputFile, 
+            DirectoryInfo outputDirectory,
+            OutputType ouputType = OutputType.Yaml)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public enum OutputType
+    {
+        Unknown,
+        Json,
+        Csv,
+        Yaml
+    }
+}
