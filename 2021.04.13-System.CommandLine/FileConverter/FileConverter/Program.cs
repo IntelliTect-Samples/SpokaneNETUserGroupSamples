7203f0291940efcf600e7498088a7c506d4151ec
Kevin B <Keboo@users.noreply.github.com>
2021-04-13 17:18:49 -0700

    System.command line (#4)
    * WIP
    
    * Adding console output
    
    * Starting work on notebook
    
    * Adding aliases
    
    * Adding model binding
    
    * Null ref fixing and vscode debug launcher
    
    * Fixing quoted nuget package.
    
    * Adding parse delegate sample
    
    * Records and simpler command handlers
    
    * Adding custom suggestions

---
+++ 2021.04.13-System.CommandLine/FileConverter/FileConverter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.CommandLine;
+using System.CommandLine.IO;
+using System.IO;
+
+namespace FileConverter
+{
+    class Program
+    {
+        /// <summary>
+        /// Demo app for converting files
+        /// </summary>
+        /// <param name="inputFile">The file to convert</param>
+        /// <param name="outputDirectory">The output directory to covert files into.</param>
+        /// <param name="outputType">The conversion type.</param>
+        /// <param name="console">Generated console.</param>
+        static void Main(
+            FileInfo inputFile, 
+            DirectoryInfo outputDirectory,
+            OutputType outputType = OutputType.Yaml,
+            IConsole console = null)
+        {
+            console.Out.WriteLine($"Processing {inputFile?.FullName} ({outputType}) => {outputDirectory?.FullName}");
+        }
+    }
+
+    public enum OutputType
+    {
+        Unknown,
+        Json,
+        Csv,
+        Yaml
+    }
+}
