e8b875bc3035f0f470d7e800579cd7ecc3ee26da
Kevin Bost <kitokeboo@gmail.com>
2021-09-14 08:57:37 -0700

    NRT Starting point

---
+++ 2021.09.14-NullableReferenceTypes/Nullability/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullability;
+
+public class Person : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+    private string _FirstName;
+    public string FirstName
+    {
+        get => _FirstName;
+        set
+        {
+            if (_FirstName != value)
+            {
+                _FirstName = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(FirstName)));
+            }
+        }
+    }
+
+    public string LastName { get; init; }
+
+    public DateTime DateOfBirth { get; }
+}
